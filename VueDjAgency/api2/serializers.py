b0de5b3c9e8183b48562fdf196ea9015192ef4bc
happine2s <happine2s@sookmyung.ac.kr>
2022-07-23 05:52:40 +0900

    출력 포맷 맞추기 ImageField까지

--- VueDjAgency/api2/serializers.py
+++ VueDjAgency/api2/serializers.py
@@ -1,14 +1,19 @@
+from dataclasses import field
 from importlib.metadata import files
+from pyexpat import model
+from unicodedata import category
 from django.contrib.auth.models import User
 from rest_framework import serializers
-from blog.models import Post, Comment
+from blog.models import Post, Comment, Category, Tag
 
 class UserSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = User
         fields = ['url', 'username', 'email', 'is_staff']
 
+
 class PostListSerializer(serializers.ModelSerializer):
+    category=serializers.CharField(source='category.name')
     class Meta:
         model = Post
         #fields='__all__'
@@ -24,8 +29,26 @@ class CommentSerializer(serializers.ModelSerializer):
         model = Comment
         fields = '__all__'
 
-class PostLikeSerializer(serializers.ModelSerializer):
+# views.py의 PATCH 메소드
+# class PostLikeSerializer(serializers.ModelSerializer):
+    # class Meta:
+    #     model = Post
+    #     #fields='__all__'
+    #     fields = ['like']
+
+class CategorySerializer(serializers.ModelSerializer):
     class Meta:
-        model = Post
-        #fields='__all__'
-        fields = ['like']
+        model=Category
+        fields=['name']
+
+class TagSerializer(serializers.ModelSerializer):
+    class Meta:
+        model=Tag
+        fields=['name']
+
+class CateTagSerializer(serializers.Serializer):
+    #cateList=CategorySerializer(many=True)
+    #tagList=TagSerializer(many=True)
+
+    cateList=serializers.ListField(child=serializers.CharField())
+    tagList=serializers.ListField(child=serializers.CharField())
--- VueDjAgency/api2/views.py
+++ VueDjAgency/api2/views.py
@@ -15,10 +15,14 @@
 #     queryset = Comment.objects.all()
 #     serializer_class = CommentSerializer
 
-from rest_framework.generics import ListAPIView, RetrieveAPIView, CreateAPIView, UpdateAPIView
-from .serializers import CommentSerializer, PostLikeSerializer, PostListSerializer, PostRetrieveSerializer
-from blog.models import Post,Comment
+from collections import OrderedDict
+from rest_framework.generics import ListAPIView, RetrieveAPIView, CreateAPIView
+from .serializers import CateTagSerializer, CommentSerializer, PostListSerializer, PostRetrieveSerializer
+from blog.models import Post,Comment,Category, Tag
 from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework.generics import GenericAPIView
+from rest_framework.pagination import PageNumberPagination
 
 class PostListAPIView(ListAPIView):
     #ListAPIView는 many=True
@@ -35,22 +39,69 @@ class CommentCreateAPIView(CreateAPIView):
     queryset = Comment.objects.all()
     serializer_class = CommentSerializer
 
-class PostLikeAPIView(UpdateAPIView):
+# class PostLikeAPIView(UpdateAPIView):
+#     queryset=Post.objects.all()
+#     serializer_class=PostLikeSerializer
+
+#     #PATCH method
+#     def update(self, request, *args, **kwargs):
+#         partial = kwargs.pop('partial', False)
+#         instance = self.get_object()
+#         #data=instance.like+1
+#         data={'like':instance.like+1}
+#         serializer = self.get_serializer(instance, data=data, partial=partial)
+#         serializer.is_valid(raise_exception=True)
+#         self.perform_update(serializer)
+
+#         if getattr(instance, '_prefetched_objects_cache', None):
+#             # If 'prefetch_related' has been applied to a queryset, we need to
+#             # forcibly invalidate the prefetch cache on the instance.
+#             instance._prefetched_objects_cache = {}
+
+#         return Response(data['like'])
+
+class PostLikeAPIView(GenericAPIView):
     queryset=Post.objects.all()
-    serializer_class=PostLikeSerializer
 
-    def update(self, request, *args, **kwargs):
-        partial = kwargs.pop('partial', False)
+    #GET method
+    def get(self, request, *args, **kwargs):
         instance = self.get_object()
-        #data=instance.like+1
-        data={'like':instance.like+1}
-        serializer = self.get_serializer(instance, data=data, partial=partial)
-        serializer.is_valid(raise_exception=True)
-        self.perform_update(serializer)
+        instance.like+=1
+        instance.save()
+        return Response(instance.like)
 
-        if getattr(instance, '_prefetched_objects_cache', None):
-            # If 'prefetch_related' has been applied to a queryset, we need to
-            # forcibly invalidate the prefetch cache on the instance.
-            instance._prefetched_objects_cache = {}
+class CateTagAPIView(APIView):
+    def get(self, request, *args, **kwargs):
+        cateList=Category.objects.all()
+        tagList=Tag.objects.all()
+        data={
+            'cateList': cateList,
+            'tagList': tagList
+        }
+        serializer=CateTagSerializer(instance=data)
+        return Response(serializer.data)
 
-        return Response(data['like'])
+class PostPageNumberPagination(PageNumberPagination):
+    page_size=3
+    #page_size_query_param='page_size'
+    #max_page_size=1000
+
+    def get_paginated_response(self, data):
+        return Response(OrderedDict([
+            ('postList',data),
+            ('pageCnt',self.page.paginator.num_pages),
+            ('curPage',self.page.number),
+        ]))
+
+class PostListAPIView(ListAPIView):
+    #ListAPIView는 many=True
+    queryset = Post.objects.all()
+    serializer_class = PostListSerializer
+    pagination_class=PostPageNumberPagination
+
+    def get_serializer_context(self):
+        return {
+            'request': None,
+            'format': self.format_kwarg,
+            'view': self
+        }
